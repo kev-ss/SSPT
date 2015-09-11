cb3c1091aa9fe7adb0902d505d436423a2c27c5b
kev-ss <kevin.petri9@icloud.com>
2015-09-11 07:24:32 +0200

    Added a test py Script in -Main- folder

---
+++ Main/lunah.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from time import strftime
+from datetime import datetime
+import ephem
+
+now = datetime.now()
+d1 = ephem.next_full_moon(now)
+d2 = ephem.next_new_moon(now)
+d3 = ephem.next_first_quarter_moon(now)
+d4 = ephem.next_last_quarter_moon(now)
+print (d2), "	| Next new Moon"
+print (d3), "	| Next quarter Moon"
+print (d1), "	| Next full Moon"
+print (d4), "	| Next last quarter Moon"
+
+m=ephem.Moon(now)
+epoch=('2015')
+print "Moon is actually: ", (ephem.constellation(m))
+
+quit()
